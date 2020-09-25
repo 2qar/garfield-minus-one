5e2ea5bd47ce980d8b7027d4feca1becf642e1a6
tuckerrrrrrrrrr <tucker1014@hotmail.com>
2020-09-24 19:30:08 -0700

    grab comics from new site

--- get_comic.sh
+++ get_comic.sh
@@ -2,16 +2,23 @@
 # Remove the third panel from today's Garfield comic and put it on Twitter
 
 TMP_GARF="/tmp/garf.gif"
+NORMAL_HEIGHT=258
 
-wget -q https://d1ejxu6vysztl5.cloudfront.net/comics/garfield/$(date +%Y)/$(date +%Y-%m-%d).gif -O $TMP_GARF
-size=$(file $TMP_GARF | cut -d',' -f3 | cut -c 2-)
+comic_url="$(curl -s "https://www.gocomics.com/garfield/$(date +%Y/%m/%d)" | egrep -o -m1 "https://assets\.amuniversal\.com/[0-9|a-z]{32}")"
+if [ -z "$comic_url" ]; then
+	echo "no comic for today, i guess"
+	exit 1
+fi
+
+wget -q "$comic_url" -O $TMP_GARF
+size=$(identify -ping -format '%w %h' $TMP_GARF)
 width=$(echo $size | cut -d' ' -f1)
-height=$(echo $size | cut -d' ' -f3)
-if ! (( width == 1200 && height < 400)); then
-    echo BAD SIZE
+height=$(echo $size | cut -d' ' -f2)
+if (( height > NORMAL_HEIGHT)); then
+    echo "BAD SIZE"
     exit 1
 fi
-convert $TMP_GARF -crop 796x$height+0+0 +repage $TMP_GARF
+convert $TMP_GARF -crop $(( width - width / 3 ))x$height+0+0 +repage $TMP_GARF
 
 total_bytes=$(wc -c $TMP_GARF | cut -d' ' -f1)
 media_id=$(twurl -H upload.twitter.com "/1.1/media/upload.json" -d "command=INIT&media_type=image/gif&total_bytes=$total_bytes" | jq -r .media_id_string)
